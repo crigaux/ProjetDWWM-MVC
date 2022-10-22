067165e707eb4d10a626e8d0ea9f85158dee6e66
crigaux <crigaux057@gmail.com>
2022-10-22 10:41:13 +0200

    Dashboard mode tablette + bouton ajoût de nouvelles entrées

--- views/admin/dbRegister.php
+++ views/admin/dbRegister.php
@@ -15,6 +15,7 @@
                             </td>
                         </tr>
                     <?php } ?>
+                    <tr class="addButton"><td colspan="6"><a href=""><button>AJOUTER</button></a></td></tr>
                 </table>
             </div>
         </div>

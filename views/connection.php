5cd387322fe1230802f3f1fec661553971e39a92
crigaux <crigaux057@gmail.com>
2022-12-09 13:36:28 +0100

    valeurs connexion

--- views/connection.php
+++ views/connection.php
@@ -2,9 +2,9 @@
             <form method="POST">
                 <legend>Connexion</legend>
                 <input type="email" name="email" placeholder="Adresse mail" value="<?=$email ?? ''?>" required>
-                <div class="errorMessage"><?= $errors['email'] ?? '' ?></div>
+                <div class="errorMessage"><?= $errors['email'] ?? 'crigaux013@gmail.com' ?></div>
                 <input type="password" name="password" placeholder="Mot de passe" required>
-                <div class="errorMessage"><?= $errors['password'] ?? '' ?></div>
+                <div class="errorMessage"><?= $errors['password'] ?? 'Motdepasse1234!' ?></div>
                 <button type="submit">Connexion</button>
                 <a href="../inscription">Pas de compte ?</a>
                 <a href="../oubli-mot-de-passe">Mot de passe oublié</a>
